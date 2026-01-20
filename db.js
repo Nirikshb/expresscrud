a164dd925f8e399bab6eb6f61724ba1e6de44cd1
Niriksh Bharti <nirikshoxicated@gmail.com>
2026-01-20 22:05:49 +0530

    Initial commit

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: 'Niriksh098&',
-  database: 'express_database'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
